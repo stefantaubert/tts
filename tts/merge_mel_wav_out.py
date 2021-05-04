385507ee1e550b550fefec224286ffe760ac6536
Stefan Taubert <stefan.taubert@posteo.de>
2021-05-04 11:54:24 +0200

    .

--- tts/merge_mel_wav_out.py
+++ tts/merge_mel_wav_out.py
@@ -1,6 +1,7 @@
+import datetime
 import os
 from logging import getLogger
-from shutil import copytree
+from shutil import copyfile, copytree
 from typing import Optional
 
 from tts.defaults import DEFAULT_MEL_IN_JSON, DEFAULT_WAV_IN_JSON
@@ -16,21 +17,25 @@ def merge_mel_and_wav_out(base_dir: str, custom_name: Optional[str] = None, mel_
   mel_info = parse_json(mel_in_json)
   wav_info = parse_json(wav_in_json)
 
-  mel_name = mel_info["name"]
+  #mel_name = mel_info["name"]
   mel_root_dir = mel_info["root_dir"]
-  wav_name = wav_info["name"]
+  #wav_name = wav_info["name"]
   wav_root_dir = wav_info["root_dir"]
 
-  merge_name = f"{mel_name}+{wav_name}"
+  merge_name = f"{datetime.datetime.now():%d.%m.%Y_%H-%M-%S}"
   if custom_name is not None:
-    merge_name = f"{custom_name}=={merge_name}"
+    merge_name = f"{merge_name}_{custom_name}"
   merge_dir = os.path.join(base_dir, merge_name)
   if os.path.isdir(merge_dir):
     print("Already created.")
   else:
     os.makedirs(merge_dir, exist_ok=False)
-    mel_dest_dir = os.path.join(merge_dir, f"mel_{mel_name}")
-    wav_dest_dir = os.path.join(merge_dir, f"wav_{wav_name}")
+    mel_dest_dir = os.path.join(merge_dir, f"mel")
+    wav_dest_dir = os.path.join(merge_dir, f"wav")
+    mel_info_file = os.path.join(merge_dir, "mel_info.json")
+    wav_info_file = os.path.join(merge_dir, "wav_info.json")
+    copyfile(mel_in_json, mel_info_file)
+    copyfile(wav_in_json, wav_info_file)
     copytree(mel_root_dir, mel_dest_dir)
     copytree(wav_root_dir, wav_dest_dir)
     print("Done.")
--- tts/utils.py
+++ tts/utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import Dict
 
 
 def parse_json(path: str) -> dict:
@@ -7,3 +8,7 @@ def parse_json(path: str) -> dict:
   with open(path, 'r', encoding='utf-8') as f:
     tmp = json.load(f)
   return tmp
+
+def save_json(path: str, mapping_dict: Dict) -> None:
+  with open(path, 'w', encoding='utf-8') as f:
+    json.dump(mapping_dict, f, ensure_ascii=False, indent=2)
