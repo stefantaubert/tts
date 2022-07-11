25e3a5aab80f8a937709e90ce30911d7141ed8a0
Stefan Taubert <23339395+stefantaubert@users.noreply.github.com>
2022-07-11 17:26:23 +0200

    .

--- src/cli.py
+++ src/cli.py
@@ -1,8 +1,11 @@
+import logging
 import os
 from argparse import ArgumentParser
+from pathlib import Path
 import sys
 
 from tts.defaults import DEFAULT_MEL_IN_JSON, DEFAULT_WAV_IN_JSON
+from tts.join_wavs import join_audios
 from tts.merge_mel_wav_out import merge_mel_and_wav_out
 
 BASE_DIR_VAR = "base_dir"
@@ -13,6 +16,18 @@ def init_merge_mel_and_wav_out_parser(parser: ArgumentParser):
   parser.add_argument('--mel_in_json', type=str, default=DEFAULT_MEL_IN_JSON)
   parser.add_argument('--wav_in_json', type=str, default=DEFAULT_WAV_IN_JSON)
   return merge_mel_and_wav_out
+
+
+def init_join_wavs_parser(parser: ArgumentParser):
+  parser.add_argument('directory', type=Path)
+  parser.add_argument('output_file', metavar="output-file", type=Path)
+  parser.add_argument('-sf', '--silence-files', type=float,
+                      help="Silence duration (in s) to insert while joining files in a directory.", default=0.2)
+  parser.add_argument('-sd', '--silence-directories', type=float,
+                      help="Silence duration (in s) to insert while joining joined files from a directory.", default=1.0)
+  parser.add_argument('--dirs-before-files', action="store_true")
+  parser.add_argument('-o', '--overwrite', action="store_true")
+  return join_audios
 
 
 def add_base_dir(parser: ArgumentParser):
@@ -34,8 +49,28 @@ def _init_parser():
   subparsers = result.add_subparsers(help='sub-command help')
 
   _add_parser_to(subparsers, "merge", init_merge_mel_and_wav_out_parser)
+  _add_parser_to(subparsers, "join-wavs", init_join_wavs_parser)
 
   return result
+
+
+def configure_logger() -> None:
+  loglevel = logging.DEBUG if __debug__ else logging.INFO
+  main_logger = logging.getLogger()
+  main_logger.setLevel(loglevel)
+  main_logger.manager.disable = logging.NOTSET
+  if len(main_logger.handlers) > 0:
+    console = main_logger.handlers[0]
+  else:
+    console = logging.StreamHandler()
+    main_logger.addHandler(console)
+
+  logging_formatter = logging.Formatter(
+    '[%(asctime)s.%(msecs)03d] (%(levelname)s) %(message)s',
+    '%Y/%m/%d %H:%M:%S',
+  )
+  console.setFormatter(logging_formatter)
+  console.setLevel(loglevel)
 
 
 def _process_args(args):
@@ -45,6 +80,7 @@ def _process_args(args):
 
 
 if __name__ == "__main__":
+  configure_logger()
   main_parser = _init_parser()
 
   received_args = main_parser.parse_args()
